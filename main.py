d153c16133b003836ce38910627bfd1c8f86d9f2
815382636 <815382636@qq.com>
2021-11-16 21:24:19 +0800

    爬取conectedpaper 医疗关键词

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
 from scrapy_certain import ScrapyCe
 from scrapy_html import ScrapyMe
 
+# words = ['Intracerebral injury',
+#          'Intracerebral hemorrhage,traumatic',
+#           'Intracerebral hemorrhage in cerebellum',
+words = ['Intracerebral hemorrhage In parenchyma',
+         'Intracerebral hemorrhage hypertensive',
+         'Intrarentricular hemorrhage',
+         'Intrarentricular conducition delay',
+         'Intrarentricular hemorrhage neonatal',
+         'Intrarentricular hemorrhage of prematurity',
+         'Intrarentricular sepatal defect',
+         'Intrarentricular Fraction',
+         'Intrarentricular hemorrhage',
+         'Intrarentricular hemorrhage',
+         'Intrarentricular conducition delay',
+         'Intrarentricular hemorrhage neonatal',
+         'Intrarentricular hemorrhage of prematurity',
+         'Intrarentricular sepatal defect',
+         'Intrarentricular Fraction',
+         'Intrarentricular Opacification',
+         'Intrarentricular hemorrhage',
+         'Sroke',
+         'Stroke Acute',
+         'Stroke Sequelae',
+         'Stroke ischemic',
+         'Stroke syndrome',
+         'Stroke Lacunar',
+         'Stroke hemorrhagic',
+         'Stroke thrombotic',
+         'Stroke cardiovascular',
+         'Cerebrovascular disease',
+         'Cerebrovascular disease sequelae',
+         'Cerebrovascular disease small ressel',
+         'Cerebral Palsy',
+         'Cerebral infarction',
+         'Cerebral ischemia',
+         'Cerebral edema',
+         'Cerebral hemorrhage',
+         'Cerebral vasospasm',
+         'Cerebral stroke',
+         'Cerebral tumor',
+         'Cerebral palsy',
+         'Cerebral spastic',
+         'Intracerebral hemorrhage',
+         'Intracerebral hematoma'
+         ]
+
 
 def main():
     key_word = input("1. 关键字查询 \n2. 具体网址查询 \n请选择查询方式：")
     if key_word == '1':
-        key_word = input("请输入查询关键字：")
+        # key_word = input("请输入查询关键字：")
+        key_words = words
         url = 'https://www.connectedpapers.com/'
-        sc = ScrapyMe(url, key_word.strip())
+        sc = ScrapyMe(url, key_words)
         sc.scrapy_mess()
     elif key_word == '2':
         url = input("请输入url地址：")
--- scrapy_html.py
+++ scrapy_html.py
@@ -1,10 +1,16 @@
+import csv
+import time
+from openpyxl import load_workbook
 from selenium import webdriver
 from openpyxl import Workbook
 from translate import translate
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ScrapyMe(object):
-    def __init__(self, url, key_word):
+    def __init__(self, url, key_words):
         chrome_options = webdriver.ChromeOptions()
         # 使用headless无界面浏览器模式
         # chrome_options.add_argument('--headless')
@@ -14,42 +20,85 @@ class ScrapyMe(object):
         self.browser = webdriver.Chrome(options=chrome_options)
         self.browser.implicitly_wait(10)
         self.url = url
-        self.key_word = key_word
+        self.key_words = key_words
 
     def scrapy_mess(self):
         self.browser.get(self.url)
         # 检索
-        self.browser.find_element_by_xpath('//input[@class="autocomplete-searchbar input-field-button"]').send_keys(
-            self.key_word)
-        self.browser.find_element_by_xpath('//div[@class="outlined-slot"]').click()
-        # 检索内容
-        content = self.search_mess()
-        # xlsx保存
-        self.store_mess(content)
-        print("scrapy success !")
+        for key_word in self.key_words:
+            # self.browser.find_element_by_xpath('//input[@class="autocomplete-searchbar input-field-button"]').send_keys(
+            #     key_word)
+            self.browser.get(f"https://www.connectedpapers.com/search?q={key_word.replace('%', ' ')}")
+            # self.browser.find_element_by_xpath('//div[@class="outlined-slot"]').click()
+            # 检索内容
+            content = self.search_mess()
+            print(key_word, len(content))
+            # xlsx保存
+            self.store_mess(content, key_word)
+            print(key_word + "scrapy success !")
 
     def search_mess(self):
         wl = []
-        for i in range(10):
+        # for q in range(33):
+        while True:
             articles = self.browser.find_elements_by_xpath('//article[@class="search-result-component shadowed-box"]')
             for article in articles:
                 title = article.find_element_by_xpath('.//span[@class="paper-title"]').text
                 authors = article.find_element_by_xpath('.//div[@class="search-result-authors"]').text
                 cites = article.find_element_by_xpath('.//div[@class="search-result-meta"]').text
-                ab = article.find_element_by_xpath('.//div[@class="search-result-abstract folded"]/div').text
-                translate_title, translate_ab = None, None
-                if title:
-                    translate_title = translate(title)
-                if ab:
-                    translate_ab = translate(ab)
-                wl.append([title, authors, cites, ab, translate_title, translate_ab])
-            self.browser.find_elements_by_xpath('//div[@class="page-selector flexrow"]/a')[-1].click()
+                a = cites.split('.')
+                b, c = a[0], a[-1]
+                href = article.find_elements_by_xpath('.//div[@class="search-result-links flexrow links-box"]/a')[
+                    0].get_property('href')
+                # print(href)
+                # ab = article.find_element_by_xpath('.//div[@class="search-result-abstract folded"]/div').text
+                # translate_title, translate_ab = None, None
+                # if title:
+                #     translate_title = translate(title)
+                # if ab:
+                #     translate_ab = translate(ab)
+                # wl.append([title, authors, cites, href, ab, translate_title, translate_ab])
+                wl.append([title, authors, b, c, href])
+            try:
+                if self.browser.find_elements_by_xpath('//div[@class="page-selector flexrow"]/a')[-1].get_property(
+                        'href') == self.browser.current_url:
+                    break
+            except:
+                break
+            try:
+                self.browser.find_elements_by_xpath('//div[@class="page-selector flexrow"]/a')[-1].click()
+            except:
+                break
         return wl
 
-    def store_mess(self, content):
-        wb = Workbook()
-        ws = wb.active
-        ws.append(['title', 'authors', 'cites', 'abstract', 'translate_title', 'translate_abstract'])
-        for c in content:
-            ws.append(c)
-        wb.save(f"{self.key_word.replace(' ', '_')}.xlsx")
+    def store_mess(self, content, keyword):
+        # wb = Workbook()
+        # ws = wb.active
+        # # ws.append(['title', 'authors', 'cites', 'href', 'abstract', 'translate_title', 'translate_abstract'])
+        # ws.append(['title', 'authors', 'periodical', 'cites', 'href'])
+        # for c in content:
+        #     ws.append(c)
+        # wb.save(f"{keyword.replace(' ', '_')}.xlsx")
+        with open('medical.csv', 'a') as wf:
+            writer = csv.writer(wf)
+            writer.writerows(content)
+        print(f"{keyword} to csv success")
+
+
+if __name__ == '__main__':
+    # with open('medical.csv', 'w') as wf:
+    #     writer = csv.writer(wf)
+    #     writer.writerow(['title', 'authors', 'periodical', 'cites', 'href'])
+    with open('medical.csv', 'r') as rf:
+        reader = csv.reader(rf)
+        s = set()
+        me = []
+        for i in reader:
+            if i[0] not in s:
+                me.append(i)
+                s.add(i[0])
+        with open('nmedical.csv', 'w') as wf:
+            writer = csv.writer(wf)
+            writer.writerows(me)
+
+
